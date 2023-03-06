f5825ab2f762e41d1f34de0f43dae328ea86cec1
pitpat.eth <kisbalazs.patrik@icloud.com>
2023-03-06 16:07:57 +0100

    init (#2)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useState } from 'react';
 
 export default function Home() {
 
@@ -14,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
     </>
   )
 }
